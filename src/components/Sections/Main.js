c02ffa2c7fd579a67f53f2ff20822adeafe407d4
chaman-rawat <nakulrawat2001@gmail.com>
2022-06-16 22:40:21 +0530

    feat: Added margin to sections

--- src/components/Sections/Main.js
+++ src/components/Sections/Main.js
@@ -2,10 +2,11 @@ import Hero from "./Hero";
 import Shorten from "./Shorten";
 import Statistics from "./Statistics";
 import GetStarted from "./GetStarted";
+import classes from "./Main.module.css";
 
 const Main = () => {
   return (
-    <main>
+    <main className={classes.main}>
       <Hero />
       <Shorten />
       <Statistics />
