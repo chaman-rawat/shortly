43bfb952568591ad637b4ad6d65bd0a86b94668a
chaman-rawat <nakulrawat2001@gmail.com>
2022-06-16 18:00:33 +0530

    feat: Added hamburger menu for smaller screen sizes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,39 @@
+import { useState } from "react";
+
 import logo from "../../images/logo.svg";
+import Button from "../UI/Button";
 import classes from "./Header.module.css";
 
 const Header = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const hamburgerHandler = () => {
+    setIsVisible((prev) => !prev);
+  };
+
   return (
     <header className={classes.header}>
-      <div className="container">
-        <img src={logo} alt="Shortly" className={classes.header__logo} />
-        <div className={classes.header__hamburger}></div>
-        <nav className={`${classes.header__nav}`}>
-          <ul className="row">
+      <div className="container row">
+        <div className={`row ${classes.header__icons}`}>
+          <img src={logo} alt="Shortly" className={classes.header__logo} />
+          <div
+            className={classes.header__hamburger}
+            onClick={hamburgerHandler}
+          ></div>
+        </div>
+
+        <nav className={`row ${classes.nav} ${!isVisible && classes.hidden}`}>
+          <ul className={`row ${classes["nav__list"]}`}>
             <li>Features</li>
             <li>Pricing</li>
             <li>Resources</li>
           </ul>
-          <ul className="row">
+          <div className={classes.nav__line} />
+          <ul className={`row ${classes["nav__list"]}`}>
             <li>Login</li>
-            <li>Sign Up</li>
+            <li>
+              <Button className={classes.btn}>Sign Up</Button>
+            </li>
           </ul>
         </nav>
       </div>
