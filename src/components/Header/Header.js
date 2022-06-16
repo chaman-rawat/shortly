ff97aa7a2228c9dfab74f6116ef04b918d718624
chaman-rawat <nakulrawat2001@gmail.com>
2022-06-16 14:55:33 +0530

    feat: Added Responsive Layout in Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,13 @@
 import logo from "../../images/logo.svg";
+import classes from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header>
-      <div className="container row">
-        <img src={logo} alt="Shortly" />
-        <nav className="row">
+    <header className={classes.header}>
+      <div className="container">
+        <img src={logo} alt="Shortly" className={classes.header__logo} />
+        <div className={classes.header__hamburger}></div>
+        <nav className={`${classes.header__nav}`}>
           <ul className="row">
             <li>Features</li>
             <li>Pricing</li>
