6fa9764268b28e124a8c325d328a033993068ee0
chaman-rawat <nakulrawat2001@gmail.com>
2022-06-16 22:39:19 +0530

    fix: Importing fonts and styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+// import "./App.css";
 import Header from "./components/Header/Header";
 import Main from "./components/Sections/Main";
 import Footer from "./components/Footer/Footer"
