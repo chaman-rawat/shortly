ca556b5ed3acd3fc5abf01dda63bc3477bcc1f24
chaman-rawat <nakulrawat2001@gmail.com>
2022-06-16 15:39:07 +0530

    feat: Added Button style

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,5 +1,11 @@
+import classes from "./Button.module.css";
+
 const Button = (props) => {
-  return <button className={props.className} type="button">{props.children}</button>;
+  return (
+    <button className={`${classes.button} ${props.className}`} type="button">
+      {props.children}
+    </button>
+  );
 };
 
 export default Button;
