7329aa74131e023b1558a64a488a1a134e6ec435
chaman-rawat <nakulrawat2001@gmail.com>
2022-06-16 22:41:03 +0530

    fix: Style of Shorten form

--- src/components/Sections/Shorten.js
+++ src/components/Sections/Shorten.js
@@ -6,7 +6,7 @@ const Shorten = () => {
     <section className={classes.shorten}>
       <div className="container center">
         <form action="" className={`row ${classes.shorten__form}`}>
-          <input className={classes.shorten__search} type="text" />
+          <input className={classes.shorten__search} placeholder="Shorten a link here..." type="text" />
           <Button className={classes.shorten__btn}>Shorten It!</Button>
         </form>
       </div>
