01bccbd9d56ff1321c7b980c540787e7b2557360
chaman-rawat <nakulrawat2001@gmail.com>
2022-06-17 14:13:20 +0530

    feat: Added mobile screen layout for Statistics

--- src/components/Sections/Statistics.js
+++ src/components/Sections/Statistics.js
@@ -1,42 +1,62 @@
 import brandRecognition from "../../images/icon-brand-recognition.svg";
 import detailedRecords from "../../images/icon-detailed-records.svg";
 import fullyCustomizable from "../../images/icon-fully-customizable.svg";
+import classes from "./Statistics.module.css";
 
 const Statistics = () => {
   return (
-    <section>
+    <section className={classes.stats}>
       <div className="container">
         <h2 className="center">Advanced Statistics</h2>
         <p className="center">
           Track how your links are performing across the web with our advanced
           statistics dashboard.
         </p>
-        <div className="row">
+        <div className={`row ${classes.stats__boxes}`}>
+          <hr className={classes.stats__box__line} />
+          <article className={classes.stats__box}>
+            <div className={classes.stats__box__logo}>
+              <img
 
-        <article>
-          <img src={brandRecognition} alt="Brand recognition" />
-          <h3>Brand Recognition</h3>
-          <p>
-            Boost your brand recognition with each click. Generic links don’t
-            mean a thing. Branded links help instil confidence in your content.
-          </p>
-        </article>
-        <article>
-          <img src={detailedRecords} alt="Detailed records" />
-          <h3>Detailed Records</h3>
-          <p>
-            Gain insights into who is clicking your links. Knowing when and
-            where people engage with your content helps inform better decisions.
-          </p>
-        </article>
-        <article>
-          <img src={fullyCustomizable} alt="Fully customizable" />
-          <h3>Fully Customizable</h3>
-          <p>
-            Improve brand awareness and content discoverability through
-            customizable links, supercharging audience engagement.
-          </p>
-        </article>
+                src={brandRecognition}
+                alt="Brand recognition"
+              />
+            </div>
+            <h3>Brand Recognition</h3>
+            <p>
+              Boost your brand recognition with each click. Generic links don’t
+              mean a thing. Branded links help instil confidence in your
+              content.
+            </p>
+          </article>
+          <article className={classes.stats__box}>
+            <div className={classes.stats__box__logo}>
+              <img
+                src={detailedRecords}
+                alt="Detailed records"
+              />
+            </div>
+            <h3>Detailed Records</h3>
+            <p>
+              Gain insights into who is clicking your links. Knowing when and
+              where people engage with your content helps inform better
+              decisions.
+            </p>
+          </article>
+          <article className={classes.stats__box}>
+            <div className={classes.stats__box__logo}>
+              <img
+
+                src={fullyCustomizable}
+                alt="Fully customizable"
+              />
+            </div>
+            <h3>Fully Customizable</h3>
+            <p>
+              Improve brand awareness and content discoverability through
+              customizable links, supercharging audience engagement.
+            </p>
+          </article>
         </div>
       </div>
     </section>
