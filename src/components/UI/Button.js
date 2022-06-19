5258527d94bf41801c7c4bc514fac961f9b40d6b
chaman-rawat <nakulrawat2001@gmail.com>
2022-06-19 16:35:18 +0530

    feat: Added layout and onClick prop to Button

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -2,7 +2,11 @@ import classes from "./Button.module.css";
 
 const Button = (props) => {
   return (
-    <button className={`${classes.button} ${props.className}`} type="button">
+    <button
+      className={`${classes.button} ${props.className}`}
+      type={props.type || "button"}
+      onClick={props.onClick}
+    >
       {props.children}
     </button>
   );
